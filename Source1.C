88194400853b1555679f978a1dbd32ef7ec589e9
Vladislav <134309915+Vanilla115@users.noreply.github.com>
2023-12-09 12:32:19 +0300

    Add files via upload

--- Source1.C
+++ Source1.C
@@ -1,10 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 
-#define MAXCHAR 1000 
+#define MAXCHAR 10000 
 #include <stdio.h> 
 #include <string.h> 
 #include <SDL.h> 
 #include <stdbool.h> 
-#include <SDL_TTF.h>
+#include <SDL_ttf.h>
+#include "FileCounter.h"
 
 struct DataFile {
     int time;
@@ -12,8 +13,8 @@ struct DataFile {
     int date;
 };
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 500;
+const int SCREEN_WIDTH = 1100;
+const int SCREEN_HEIGHT = 500;//500
 
 
 void drawGraph(SDL_Renderer* renderer, struct DataFile* DataMass, int counter) {
@@ -28,23 +29,41 @@ void drawGraph(SDL_Renderer* renderer, struct DataFile* DataMass, int counter) {
         }
     }
 
+
+    // Разметка X
+    int rangeX = 50;
+    for (int i = 0; i < 10; i++) {
+        SDL_SetRenderDrawColor(renderer, 64, 64, 64, 64);
+        SDL_RenderDrawLine(renderer, rangeX + 70, SCREEN_HEIGHT - 50, rangeX + 70, 50);
+        rangeX += 70;
+    }
+    // Разметка Y
+    int rangeY = 50;
+    for (int i = 0; i < 10; i++) {
+        SDL_SetRenderDrawColor(renderer, 64, 64, 64, 64);
+        SDL_RenderDrawLine(renderer, 50, rangeY + 40, SCREEN_WIDTH - 350, rangeY + 40);
+        rangeY += 40;
+    }
+
     // Отрисовать график 
+    // Цвет
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
     // Отрисовать горизонтальную ось 
-    SDL_RenderDrawLine(renderer, 50, SCREEN_HEIGHT - 50, SCREEN_WIDTH - 50, SCREEN_HEIGHT - 50);
+    SDL_RenderDrawLine(renderer, 50, SCREEN_HEIGHT - 50, SCREEN_WIDTH - 350, SCREEN_HEIGHT - 50);
 
     // Отрисовать вертикальную ось 
     SDL_RenderDrawLine(renderer, 50, SCREEN_HEIGHT - 50, 50, 50);
 
 
-    int graphWidth = SCREEN_WIDTH - 100;
+    int graphWidth = SCREEN_WIDTH - 400;
     int graphHeight = SCREEN_HEIGHT - 100;
 
-    int XxCoordinates[5000];
-    int XyCoordinates[5000];
-    int YxCoordinates[5000];
-    int YyCoordinates[5000];
+
+    int* XxCoordinates = (int*)malloc(counter * sizeof(int));
+    int* XyCoordinates = (int*)malloc(counter * sizeof(int));
+    int* YxCoordinates = (int*)malloc(counter * sizeof(int));
+    int* YyCoordinates = (int*)malloc(counter * sizeof(int));
     int xCount = 0, yCount = 0;
 
     for (int i = 0; i < counter; i++) {
@@ -87,6 +106,8 @@ void drawGraph(SDL_Renderer* renderer, struct DataFile* DataMass, int counter) {
         SDL_RenderFillRect(renderer, &pointRect);
     }
 
+
+
     // TTF
     if (TTF_Init() == -1)
     {
@@ -94,77 +115,113 @@ void drawGraph(SDL_Renderer* renderer, struct DataFile* DataMass, int counter) {
     }
 
     //Стиль
-    TTF_Font* Sans = TTF_OpenFont("sample.ttf", 24);
+    TTF_Font* Sans = TTF_OpenFont("sample.ttf", 36);
     SDL_Color White = { 255, 255, 255 };
 
 
     SDL_Surface* surfaceMessage =
-        TTF_RenderText_Solid(Sans, "Data", White);
+        TTF_RenderText_Solid(Sans, "Data(Y)", White);
 
-    
+    // Message
     SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
 
-    SDL_Rect Message_rect; 
-    Message_rect.x = 27;  
-    Message_rect.y = 4; 
-    Message_rect.w = 50; 
-    Message_rect.h = 40; 
+    SDL_Rect Message_rect;
+    Message_rect.x = 27;
+    Message_rect.y = 15;
+    Message_rect.w = 80;
+    Message_rect.h = 30;
 
- 
+
     SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
 
+
+    // Message 1
+    TTF_Font* Sans1 = TTF_OpenFont("sample.ttf", 36);
+    SDL_Color White1 = { 255, 255, 255 };
+
+
+    SDL_Surface* surfaceMessage1 =
+        TTF_RenderText_Solid(Sans1, "Time(X)", White1);
+
+
+    SDL_Texture* Message1 = SDL_CreateTextureFromSurface(renderer, surfaceMessage1);
+
+    SDL_Rect Message_rect1;
+    Message_rect1.x = 350;
+    Message_rect1.y = 452;
+    Message_rect1.w = 80;
+    Message_rect1.h = 30;
+
+    SDL_RenderCopy(renderer, Message1, NULL, &Message_rect1);
+
+    //Значения
+    //Х
+    //int maximumX = maxX;
+    //int ratio = maximumX / 10;
+    //int MassRatios[10];
+    //char MassStrRatios[100];
+    //for (int i = 0; i < 10; i++) {
+    //    MassRatios[i] = maximumX - ratio;
+    //    ratio += ratio;
+    //    char str[10];
+    //    sprintf(str, "%d", MassRatios[i]);
+    //
+    ////
+    //TTF_Font* SansR = TTF_OpenFont("sample.ttf", 36);
+    //SDL_Color WhiteR = { 255, 255, 255 };
+
+    ////char X = MassStrRatios[0] + "------" + MassStrRatios[1] + "------" + MassStrRatios[2] + "------" + MassStrRatios[3] + "------" + MassStrRatios[4] + "------" + MassStrRatios[5] + "------" + MassStrRatios[6] + "------" + MassStrRatios[7] + "------" + MassStrRatios[8] + "------" + MassStrRatios[9];
+
+    //SDL_Surface* surfaceMessageR =
+    //    TTF_RenderText_Solid(SansR, sprintf(str, "%d", MassRatios[i]), WhiteR);
+
+    //SDL_Texture* MessageR = SDL_CreateTextureFromSurface(renderer, surfaceMessageR);
+    //SDL_Rect Message_rectR;
+    //Message_rectR.x = 300;
+    //Message_rectR.y = 440;
+    //Message_rectR.w = 80;
+    //Message_rectR.h = 30;
+    //SDL_RenderCopy(renderer, MessageR, NULL, &Message_rectR);
+
+    //Вывод
+    /*SDL_FreeSurface(surfaceMessageR);
+    SDL_DestroyTexture(MessageR) };*/
+    SDL_FreeSurface(surfaceMessage1);
+    SDL_DestroyTexture(Message1);
     SDL_FreeSurface(surfaceMessage);
     SDL_DestroyTexture(Message);
+
     SDL_RenderPresent(renderer);
-
-    /*
-    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(sample.ttf, "Data", color); // создать поверхность с текстом
-    SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); // создать текстуру из поверхности
-
-    int textW = 0;
-    int textH = 0;
-    TTF_SizeText(Sans, "Data", &textW, &textH); // получить размер текста
-
-    SDL_Rect Message_rect; 
-    Message_rect.x = SCREEN_WIDTH - textW;  
-    Message_rect.y = SCREEN_HEIGHT - 50 - textH; 
-    Message_rect.w = textW;
-    Message_rect.h = textH;
-
-    SDL_RenderCopy(renderer, Message, NULL, &Message_rect); 
-    SDL_RenderPresent(renderer); 
-    */
-
 }
 
 int main() {
-    struct DataFile DataMass[5000];
     FILE* fp;
     char row[MAXCHAR];
     char* token;
 
-    int counter = 0;
-
+    int counter = cntLines("Data.csv");
+    struct DataFile* DataMass = (struct DataFile*)malloc(counter * sizeof(struct DataFile));
     fp = fopen("Data.csv", "r");
 
     if (fp == NULL) {
         printf("Unable to open the file.\n");
         return 1;
     }
-
-    while (fgets(row, MAXCHAR, fp) && counter <= 5000) {
+    int i = 0;
+    while (fgets(row, MAXCHAR, fp) && i <= counter) {
         token = strtok(row, ";");
         if (token != NULL) {
-            DataMass[counter].time = atoi(token);
+            DataMass[i].time = atoi(token);
             token = strtok(NULL, ";");
 
             if (token != NULL) {
-                strncpy(DataMass[counter].name, token, sizeof(DataMass[counter].name));
+                strncpy(DataMass[i].name, token, sizeof(DataMass[i].name));
                 token = strtok(NULL, ";");
 
                 if (token != NULL) {
-                    DataMass[counter].date = atoi(token);
-                    counter++;
+                    DataMass[i].date = atoi(token);
+                    //counter++
+                    i++;
                 }
             }
         }
@@ -203,3 +260,6 @@ int main() {
 
     return 0;
 }
+
+
+// Как сделать легенду
