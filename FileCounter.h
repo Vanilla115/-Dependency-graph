d37d02f9d56619184dabe6d3d3ff83019fa4b2b1
Vladislav <134309915+Vanilla115@users.noreply.github.com>
2023-12-09 12:51:32 +0300

    Create FileCounter.h

---
+++ FileCounter.h
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#define MAXCHAR 1000 
+#include <stdio.h> 
+#include <string.h> 
+#include <stdbool.h> 
+
+
+int cntLines(const char* filename) {
+    int lines = 0;
+    int any;
+    FILE* f = fopen("Data.csv", "r");
+    if (f == NULL) {
+        return -1;
+    }
+    do {
+        any = fgetc(f);
+        //printf("%c", any);//debug
+        if (any == '\n') {
+            lines++;
+        }
+    } while (any != EOF);
+    fclose(f);
+    return lines;
+}
